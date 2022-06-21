d321cf01bdd2562bbdb92f8a9997089e0ae3f5df
Auroaruora <wengzihui020716@gmail.com>
2022-06-21 12:17:46 -0400

    canvas

--- project/changeBasis/js/changeBasis.js
+++ project/changeBasis/js/changeBasis.js
@@ -1,9 +1,14 @@
-var width = 400, height = 400;
+const width = 400, height = 400;// Hight and width of the canvas
 var sz = 6, xmin = -sz, xmax = sz, ymin = -sz, ymax = sz;
 
 var x0, y0;
 // grid parameters
 var ticks = 4, subticks = 5*ticks, dx = 1/ticks, num_pts = 120;
+
+// "Muted" qualitative color scheme suggested by Paul Tol
+// https://personal.sron.nl/~pault/
+const palette = ["#000000", "#332288", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#CC6677", "#882255", "#AA4499"];
+
 
 $(document).ready(function() {
     initialize_canvas("standardBasis", width, height);
@@ -13,3 +18,13 @@ $(document).ready(function() {
     context = d3.select("#standardBasis");
 
 });
+
+function drawStandardGrids(){
+
+    context = d3.select("#standardBasis");
+    pen.opacity(1);
+    pen.color(palette[0]).width("1px").opacity(1);
+    grid([xmin, ymin], [xmax, ymax], [subticks, subticks]);
+    pen.color(palette[0]).width("3px");
+    grid([xmin, ymin], [xmax, ymax], [ticks, ticks]);
+}
